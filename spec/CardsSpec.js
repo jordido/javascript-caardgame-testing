e0d2f9d3bf2d5975ef0d6edec2b1d1d75b1b30c2
jordido <jordi@zona-e.com>
2014-10-13 17:55:01 +0200

    refactoring, added the loop

--- spec/CardsSpec.js
+++ spec/CardsSpec.js
@@ -1,4 +1,5 @@
 function whoWins(hand1,hand2) {
+  var cardsRanking = "123456789JQK";
   var points1 = 0;
   var points2 = 0;
   if (!hand1 || !hand2) {
@@ -6,16 +7,15 @@ function whoWins(hand1,hand2) {
   } else if (!hand1[0] || !hand2[0]) {
     throw("Error, there are no cards to judge.");
   } 
-  if (hand2[0] > hand1[0]) {
-    points2 += 1;
-  } else if (hand2[0] < hand1[0]) {
-    points1 += 1;
-  }; 
-  if (hand2[1] > hand1[1]) {
-    points2 += 1;
-  } else if (hand2[1] < hand1[1]) {
-    points1 += 1;
-  };
+  var i;
+  for (i=0 ; i < hand1.length; i++) { 
+    if (cardsRanking.indexOf(hand2[i]) > cardsRanking.indexOf(hand1[i])) {
+      points2 += 1;
+    } else if (cardsRanking.indexOf(hand2[i]) < cardsRanking.indexOf(hand1[i])) {
+      points1 += 1;
+    }; 
+  }
+
   if (points2 > points1) {
      return ("Player 2 wins "+ points2 +" to "+ points1);
    } else if (points2 < points1) {
@@ -30,13 +30,14 @@ function whoWins(hand1,hand2) {
 describe("Game of Cards", function() {
 
   it("wins with one card hand", function(){
-    expect(whoWins(['1'],['2'])).toEqual("Player 2 wins 1 to 0");
+    expect(whoWins(['1'],['Q'])).toEqual("Player 2 wins 1 to 0");
     expect(whoWins(['2'],['1'])).toEqual("Player 1 wins 1 to 0");
+    expect(whoWins(['K'],['Q'])).toEqual("Player 1 wins 1 to 0");
   });
 
   it("should return tie", function() {
     expect(whoWins(["1"],["1"])).toEqual("Tie");
-    expect(whoWins(["2","1"],["1","2"])).toEqual("Tie");
+    expect(whoWins(["2","J"],["1","Q"])).toEqual("Tie");
   });
 
   it("should win with 2 cards", function(){
